66984a5f8e45137a060c04d59094ef6166a04dbc
KroegerLeif <Leif.kr@outlook.com>
2025-09-17 13:29:16 +0200

    Creation of Controller Class

---
+++ src/main/java/org/example/recapproject_todo_neuefische/controller/TodoController.java
@@ -0,0 +1,26 @@
+package org.example.recapproject_todo_neuefische.controller;
+
+import org.example.recapproject_todo_neuefische.entity.Todo;
+import org.example.recapproject_todo_neuefische.service.TodoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todo")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    //GET Mapping
+    @GetMapping
+    public List<Todo> getAllTodos(){
+        return todoService.findAll();
+    }
+}
