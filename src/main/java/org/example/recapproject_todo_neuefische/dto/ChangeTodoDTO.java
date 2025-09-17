671daa3b2cca35427db98bf60c18e39a0652cffa
KroegerLeif <Leif.kr@outlook.com>
2025-09-17 14:55:59 +0200

    new DTO so a User can change a Todo

---
+++ src/main/java/org/example/recapproject_todo_neuefische/dto/ChangeTodoDTO.java
@@ -0,0 +1,9 @@
+package org.example.recapproject_todo_neuefische.dto;
+
+import org.example.recapproject_todo_neuefische.entity.Status;
+
+public record ChangeTodoDTO(String description,
+                            String id,
+                            Status status,
+                            String title) {
+}
