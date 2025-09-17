f62021d4021d8b636d70c3fab3a3ac6dd5138c78
KroegerLeif <Leif.kr@outlook.com>
2025-09-17 15:21:28 +0200

    Changes The Values of the Todo in Service

--- src/main/java/org/example/recapproject_todo_neuefische/service/TodoService.java
+++ src/main/java/org/example/recapproject_todo_neuefische/service/TodoService.java
@@ -1,5 +1,6 @@
 package org.example.recapproject_todo_neuefische.service;
 
+import org.example.recapproject_todo_neuefische.dto.ChangeTodoDTO;
 import org.example.recapproject_todo_neuefische.dto.RegisterTodoDTO;
 import org.example.recapproject_todo_neuefische.entity.Todo;
 import org.example.recapproject_todo_neuefische.exception.UserNotFoundException;
@@ -29,6 +30,11 @@ public class TodoService {
         todoRepository.save(mapRegisterToTodo(registerTodoDTO));
     }
 
+    public void updateUser(ChangeTodoDTO changeTodoDTO){
+        Todo updatedTodo = mapChangeToTodo(changeTodoDTO);
+        todoRepository.save(updatedTodo);
+    }
+
     public Todo getTodoById(String id) throws UserNotFoundException{
         return todoRepository.findById(id).orElseThrow(
                 () -> new UserNotFoundException("No User Found with ID: " + id + "")
@@ -43,6 +49,15 @@ public class TodoService {
                                 registerTodoDTO.status(),
                                 Instant.now());
         return newTodo;
+    }
+
+    private Todo mapChangeToTodo(ChangeTodoDTO changeTodoDTO){
+        Todo updatedTodo = getTodoById(changeTodoDTO.id());
+        //Updates the Dto with the new values
+        updatedTodo.withDescription(changeTodoDTO.description());
+        updatedTodo.withStatus(changeTodoDTO.status());
+        updatedTodo.withTitle(changeTodoDTO.title());
+        return updatedTodo;
     }
 
 
