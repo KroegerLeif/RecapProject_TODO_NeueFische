ce0116bb349dd95f93244b833e3f10955b02430a
KroegerLeif <Leif.kr@outlook.com>
2025-09-17 11:47:09 +0200

    Initial commit

---
+++ src/main/java/org/example/recapproject_todo_neuefische/RecapProjectTodoNeueFischeApplication.java
@@ -0,0 +1,13 @@
+package org.example.recapproject_todo_neuefische;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapProjectTodoNeueFischeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecapProjectTodoNeueFischeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/recapproject_todo_neuefische/RecapProjectTodoNeueFischeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.recapproject_todo_neuefische;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapProjectTodoNeueFischeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
