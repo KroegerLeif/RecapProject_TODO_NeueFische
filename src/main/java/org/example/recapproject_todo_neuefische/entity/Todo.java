0530144996f13785282edf7572d06955c5bb7c70
KroegerLeif <Leif.kr@outlook.com>
2025-09-17 13:41:15 +0200

    A Todo now can save the status

--- src/main/java/org/example/recapproject_todo_neuefische/entity/Todo.java
+++ src/main/java/org/example/recapproject_todo_neuefische/entity/Todo.java
@@ -1,9 +1,13 @@
 package org.example.recapproject_todo_neuefische.entity;
 
+import lombok.With;
+
 import java.time.Instant;
 
+@With
 public record Todo(String id,
                    String title,
                    String description,
+                   Status status,
                    Instant created) {
 }
